54bebbb0ec663ef324a4aa510db17c031b0ffc2e
robert trujillo <rt1596006@gmail.com>
2026-05-09 13:24:50 -0500

    se agragó alcanse de variables

---
+++ S6_alcanse_var.cs
@@ -0,0 +1,16 @@
+
+using System.Diagnostics.Contracts;
+
+int cont = 100;
+cont ++ ;
+contar();
+contar();
+contar();
+Console. WriteLine("ver main  " + cont);
+
+void contar()
+{
+    int cont = 0;
+    cont ++ ;
+    Console. WriteLine("ver contar   " + cont);
+}
