d81f863654ea6962c48f46694c004deecb91c6da
robert trujillo <rt1596006@gmail.com>
2026-05-09 12:36:32 -0500

    agregar funcion sin datos

---
+++ S6_funcion_sin_parametros.cs
@@ -0,0 +1,8 @@
+mostarmenu();
+void mostarmenu()
+{
+    Console.WriteLine("biembenido a mi sistema ");
+    Console.WriteLine("seleccione su opcion");
+    Console.WriteLine("1. Calcular IMC");
+    Console.WriteLine("2. Salir");
+}
