4e1a84208147d1aed88806dedb7039450fdba65b
robert trujillo <rt1596006@gmail.com>
2026-05-09 10:02:37 -0500

    changes

---
+++ algoritmosCsharp/algoritmosCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+Console.WriteLine("ingrese su nota");
+double nota = double.Parse(Console.ReadLine());
+if (nota >= 12)
+{
+    Console.WriteLine("arpobado");
+}
+else
+{
+    Console.WriteLine("desaprobado");
+}
